a3e4bc87b86d63f048578ba5109d778d8de4054f
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2020-11-03 18:09:34 +0600

    arithmatic operator

--- numbers.rb
+++ numbers.rb
@@ -1,1 +1,5 @@
-p p 6.0 + 5, 6 - 5.0, 6 / 0.5, 3 * 2, 5 ** 5
+# p p 6.0 + 5, 6 - 5.0, 6 / 0.5, 3 * 2, 5 ** 5
+
+# arithmetic operator precedence
+# p 2 ** 3 / 2, 2 ** (3 / 2)
+p 5 + 15 * 20 - 2 / 6 ** 3 - (3 + 1) # parentheses, exponent, multiplication, division, addition, subtraction
