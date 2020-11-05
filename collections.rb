2a9ccd6ef6fb3a09aa3199f467e04ceacc83b6ff
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2020-11-05 19:07:29 +0600

    ruby creating initializing array, vscode settings & array delete

---
+++ collections.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# x = [1, 2]
+# p x
+
+# y = Array.new
+# y = []
+# y[0] = 100
+# y[10] = 200
+# y.push(*y)
+# y << 2
+# p y
+# p y.delete nil
+# p y
+# p y.delete_at 2
+# p y
+# y.delete_if { |i| i > 100 }
+# p y
