dc1ba91b28fc353a14fa10e23505f25f2bcb8767
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2020-11-08 17:07:32 +0600

    conditional

---
+++ conditionals.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# x = '5'
+# y = 5
+# if x.to_i == y
+#   puts 'x is equal to y'
+# else
+#   puts 'x not equal to y'
+# end
+# unless x == y
+#   puts 'x not equal to y'
+# end
+# puts 'x not equal to y' unless x == y
+
+# if x == y
+#   puts 'x is equal to y'
+# elsif x.to_i == y
+#   puts 'x is equvalent to y'
+# else
+#   puts 'x not equal to y'
+# end
+
+# if x == y && x.to_i == y
+#   puts 'x is equvalent to y'
+# end
+# puts 'x is equvalent to y' if x == y || x.to_i == y
