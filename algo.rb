0f25b0258e25187d7b61656cbbdb54adb974a8b6
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2020-11-11 11:51:52 +0600

    fibonacci sequence digit counter

--- algo.rb
+++ algo.rb
@@ -89,3 +89,19 @@ end
 # end
 
 # puts sundays
+
+def fib_digit_counter
+  # num1, num2, i = -1, 0, 1
+  num1 = -1
+  num2 = 0
+  i = 1
+  while i.to_s.length < 1000
+    num1 += 1
+
+    i, num2 = num2, num2 + i
+  end
+
+  num1
+end
+
+puts fib_digit_counter
