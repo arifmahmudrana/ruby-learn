ce7099aad8c83c8b1fee1eff5e9380a79a713b94
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2020-11-16 12:22:22 +0600

    substring & falsy

---
+++ block.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# 10.times { |i| puts (i + 1) * 3 }
+
+str = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis aperiam est numquam quisquam. Ad unde dolores vel quia dolor eligendi consequuntur ab optio, voluptas ullam consequatur molestias nulla non maiores cupiditate, repellat, repudiandae quod eius temporibus suscipit? Quos deleniti praesentium ratione laboriosam ex soluta? Sint natus quas velit tempore molestiae.'
+
+# p str[10..-10]
+# p str[-10, 5]
+
+puts 'false' if 0 # only nil and false are counted as falsy
