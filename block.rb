969f7594646dc299c69b5a73c7336bcffcb68ba8
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2020-11-16 17:58:56 +0600

    fizzbuzz

--- block.rb
+++ block.rb
@@ -7,4 +7,26 @@ str = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis aperiam
 # p str[10..-10]
 # p str[-10, 5]
 
-puts 'false' if 0 # only nil and false are counted as falsy
+# puts 'false' if 0 # only nil and false are counted as falsy
+
+def fizzbuzz(number)
+  number.times do |i|
+    i += 1
+    # case i
+    # when (i % 15).zero? then puts 'Fizzbuzz'
+    # when (i % 5).zero? then puts 'Buzz'
+    # when (i % 3).zero? then puts 'Fizz'
+    # else puts i
+    # end
+    if (i % 15).zero?
+      puts 'Fizzbuzz'
+    elsif (i % 5).zero?
+      puts 'Buzz'
+    elsif (i % 3).zero?
+      puts 'Fizz'
+    else
+      puts i
+    end
+  end
+end
+fizzbuzz(15)
