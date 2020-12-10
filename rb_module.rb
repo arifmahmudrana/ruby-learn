1217f95cb7e982f4f6881a9554815879712b4d27
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2020-12-10 15:10:01 +0600

    module sample code

--- rb_module.rb
+++ rb_module.rb
@@ -21,3 +21,38 @@ end
 class Cookie
   include Cream
 end
+
+module SomeModule
+  module_function
+
+  def param
+    @param
+  end
+
+  def param=(v)
+    @param = v
+  end
+end
+
+module MyModule
+  class << self; attr_accessor :var; end
+end
+
+module MyAnotherModule
+  class << self
+    attr_accessor :my_variable
+  end
+  self.my_variable = 2 + 2
+end
+
+module Site
+  @@name = 'StackOverflow'
+
+  def self.name=(value)
+    @@name = value
+  end
+
+  def self.name
+    @@name
+  end
+end
--- require_example.rb
+++ require_example.rb
@@ -9,5 +9,19 @@ require './rb_module'
 # cookie = Cookie.new
 # p cookie.cream?
 
-cookie = Cream::Human.new
-p cookie.no_cream?
+# cookie = Cream::Human.new
+# p cookie.no_cream?
+
+# p SomeModule.param
+# SomeModule.param = 1
+# p SomeModule.param
+
+# p MyModule.var
+# MyModule.var = 'this is saved at @var'
+# p MyModule.var
+
+# p MyAnotherModule.my_variable
+
+p Site.name
+Site.name = 'Test'
+p Site.name
